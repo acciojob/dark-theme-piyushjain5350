abd65ee9aa7814a7001501368a7751cc72d0ac15
piyushjain5350 <126599802+piyushjain5350@users.noreply.github.com>
2023-07-09 21:24:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 //your code here
 
+const bodyMain = document.getElementById("app");
+const button =document.getElementById("swap");
+
+button.addEventListener("click",swapTheme);
+
+function swapTheme(){
+	bodyMain.classList.add("night");
+	button.classList.add("button_night");
+}
